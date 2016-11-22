17bc76244abca0019dafc1618e3493159140fde6
John <jnaguib@cmu.edu>
2016-11-22 10:58:27 +0200

    design modifications

--- script.js
+++ script.js
@@ -14,7 +14,16 @@ function init(){
 	// }
 	)
 
-	var animationSpeed = 200;
+	$(".front").mouseenter(function(){
+		$(this).children(".projectTitle").css("color", "red");
+		var click = "<p class='click_text'> Click for more info </p>";
+		$(this).append(click);
+	});
+
+	$(".front").mouseleave(function(){
+		$(this).children(".projectTitle").css("color", "black");
+		$(this).children(".click_text").remove();
+	});
 
 
 }
