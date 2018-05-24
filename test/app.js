70155c34cce21f8a9a1dec9764b32c93200268e2
John <jnaguib@cmu.edu>
2018-05-24 15:43:10 +0200

    Added arabic version of shaweesh files

--- test/app.js
+++ test/app.js
@@ -9,22 +9,22 @@
          var trow = $(this);
          switch (trow.index()){
            case (0):
-              trow.append('<td> <input type="text" /> </td>');
+              trow.prepend('<td> <input type="text" /> </td>');
               break;
            case (32):
-              trow.append('<td> <button onclick=fillMonth(' + colNum + ')> Fill </button> </td>');
+              trow.prepend('<td> <button onclick=fillMonth(' + colNum + ')> املأ </button> </td>');
               break;
             case(33):
-              trow.append('<td id="foodQuantity_' + colNum + '"> 0 </td>');
+              trow.prepend('<td id="foodQuantity_' + colNum + '"> 0 </td>');
               break;
             case(34):
-              trow.append('<td> <input type="number" value=0 id="multiplier_' + colNum + '" /> </td>');
+              trow.prepend('<td> <input type="number" value=0 id="multiplier_' + colNum + '" /> </td>');
               break;
             case(35):
-              trow.append('<td id="multiplierTotal_' + colNum + '" /> </td>');
+              trow.prepend('<td id="multiplierTotal_' + colNum + '" /> </td>');
               break;
             default:
-              trow.append('<td> <input type="number" class="quantity food_' + colNum + '" value=0 /></td>');
+              trow.prepend('<td> <input type="number" class="quantity food_' + colNum + '" value=0 /></td>');
          }
 
 
